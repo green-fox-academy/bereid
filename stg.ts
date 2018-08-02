0a7d8a6a54989b0cb84a0659be944beafd078fde
bereid <berei.daniel@gmail.com>
2018-08-02 17:31:14 +0200

    a nap utolso kommitja az irodabol

--- stg.ts
+++ stg.ts
@@ -126,9 +126,24 @@ let i2: number = 3;
 // tell if i1 is higher than i2 squared and smaller than i2 cubed (as a boolean)
 console.log(i1 > i2 ** 2 && i1 < i2 ** 3);
 
-
 let j: number = 1521;
 // tell if j is dividable by 3 or 5 (as a boolean)
+console.log(j % 5 == 0 || j % 3 == 0);
+
 let k: string = 'Apple';
 // fill the k variable with its content 4 times
-console.log(k);
+console.log(k );
+
+// Write a program that stores 3 sides of a cuboid as variables (floats)
+// The program should write the surface area and volume of the cuboid like:
+// Surface Area: 600
+// Volume: 1000
+
+let cuboidSideA: number = 12
+let cuboidSideB: number = 7
+let cuboidSideC: number = 11
+
+let surfaceArea: number = (cuboidSideA * cuboidSideB * 2) + (cuboidSideB * cuboidSideC * 2) + (cuboidSideA * cuboidSideC *2)
+let cubeVolume: number = cuboidSideA * cuboidSideB * cuboidSideC
+
+console.log(surfaceArea, cubeVolume);
