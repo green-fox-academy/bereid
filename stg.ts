91ae59a96eabbae0ffd98b1fc8a1faf9e1049dd0
bereid <berei.daniel@gmail.com>
2018-08-03 11:04:20 +0200

    megvan egy ujabb task, most ugrik a majom a vizbe

--- stg.ts
+++ stg.ts
@@ -165,37 +165,63 @@ let out: number = 0;
 
 // if l is even increment out by one
 if(l % 2 == 0){
-    out++
+    out = out++
     console.log(out)
 }
 
 let n: number = 13;
 let out2: string = '';
 
-// if b is between 10 and 20 set out2 to "Sweet!"
+// if n is between 10 and 20 set out2 to "Sweet!"
 // if less than 10 set out2 to "Less!",
 // if more than 20 set out2 to "More!"
-console.log(out2);
+
+if(20 > n || n > 10){
+   out2 = "Sweet"
+   console.log(out2) 
+}
 
 let m: number = 123;
 let credits: number = 100;
 let isBonus: boolean = false;
 
 // if credits are at least 50,
-// and is_bonus is false decrement c by 2
+// and is_bonus is false decrement m by 2
 // if credits are smaller than 50,
-// and is_bonus is false decrement c by 1
-// if is_bonus is true c should remain the same
-console.log(c);
+// and is_bonus is false decrement m by 1
+// if is_bonus is true m should remain the same
+
+if(credits >= 50 && isBonus === false){
+    m = m-2
+}
+if(credits < 50 && isBonus === false){
+    m = m-1
+}
+if(isBonus){
+    m = m-0
+}
+
+console.log(m);
 
 let q: number =  8;
 let time: number = 120;
 let out3: string = '';
 
-// if d is dividable by 4
+// if q is dividable by 4
 // and time is not more than 200
 // set out3 to "check"
 // if time is more than 200
 // set out3 to "Time out"
 // otherwise set out3 to "Run Forest Run!"
+
+if(q % 4 == 0 && time <= 200){
+    out3 = "Check"
+}
+if(time > 200){
+    out3 = "Time out"
+} 
+else{
+    out3 = "Run Forest Run!"
+}
+
 console.log(out3);
