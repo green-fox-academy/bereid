56708e41bf4dc9a1553b21b62b53126f8a5e66bf
bereid <berei.daniel@gmail.com>
2018-08-03 11:20:15 +0200

    fent van a sokszorozas is, bro

--- stg.ts
+++ stg.ts
@@ -225,3 +225,13 @@ else{
 }
 
 console.log(out3);
+
+// Create a program that writes this line 100 times:
+// "I won't cheat on the exam!"
+
+let u = 1
+let iDontCheat = "I won't cheat on the exam!"
+
+for(u; u <= 100; u++){
+    console.log(iDontCheat)
+}
