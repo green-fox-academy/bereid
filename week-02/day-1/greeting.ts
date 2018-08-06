45647e5e2a6eb094d3481d280c9aa66142d00b4d
bereid <berei.daniel@gmail.com>
2018-08-06 14:26:58 +0200

    added(greetings): second task ready

---
+++ week-02/day-1/greeting.ts
@@ -0,0 +1,18 @@
+export {}
+
+// -  Create variable named `name` and assign the value `Greenfox` to it
+// -  Create a function called `greet` that greets it's input parameter
+//     -  Greeting is printing e.g. `Greetings, dear Greenfox`
+//     -  Prepare for the special case when no parameters are given
+// -  Greet `name`
+
+let name: string = "Greenfox"
+
+function greet(greetName: string = "SB"){
+    return "Greetings, dear " + greetName
+}
+
+let answer = greet(name);
+console.log(answer);
+
+greet();
