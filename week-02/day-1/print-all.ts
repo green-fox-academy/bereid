8f96edf27b996dc7283e6fe8b1ebbdf90bf940fc
bereid <berei.daniel@gmail.com>
2018-08-06 17:36:50 +0200

    added(print-all, increment-element, change-elements): micsinalok uff??

---
+++ week-02/day-1/print-all.ts
@@ -0,0 +1,9 @@
+// -  Create a variable named `integers` with the following content: `[4, 5, 6, 7]`
+// -  Log each the element of `integers` to the console*
+// *hint: use a loop, console.log(integers) won't cut it
+// -  bonus for using the correct built in array method
+
+let intergers = [4, 5, 6, 7]
+for(let i = 0; i < intergers.length; i++){
+    console.log(intergers[i]);
+}
