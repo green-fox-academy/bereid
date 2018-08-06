acb66dfaa38474b07b400359b7356d21662fc222
bereid <berei.daniel@gmail.com>
2018-08-06 09:30:54 +0200

    parametric-average: meg hianyzik belole a user input function

---
+++ week-01/day-5/average.ts
@@ -0,0 +1,15 @@
+function getAverage (a,b): number {
+    var average = (a + b) / 2;
+    console.log("Average: " + average)
+    return average
+}
+
+function getSum (a,b): number {
+    var sum = (a + b);
+    console.log("Sum: " + sum)
+    return sum
+}
+
+getAverage (7, 12), getSum (7, 12)
+
+// nem hiszem hogy ez a megoldás, de mukodik, BESZELJUK MEG
