81fae9b5641a21fba397d64f1a45cf4c711aece2
bereid <berei.daniel@gmail.com>
2018-08-06 14:03:36 +0200

    added(doubling): first task is ready, lets do the others

---
+++ week-02/day-1/doubling.ts
@@ -0,0 +1,16 @@
+// -  Create a variable named `baseNum` and assign the value `123` to it
+// -  Create a function called `doubling` that doubles it's input parameter and returns with an integer
+// -  Log the result of `doubling(baseNum)` to the console
+
+let baseNum: number = 123
+
+function doubling(inputNumber: number): number {
+    return inputNumber * 2;
+}
+
+let answer = doubling(baseNum);
+console.log(answer);
+
+
+
+
