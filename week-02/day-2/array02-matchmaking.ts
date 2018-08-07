c6e6319b8d5f72e55f2812c11b494f1649fbe668
bereid <berei.daniel@gmail.com>
2018-08-07 15:04:32 +0200

    added(matchmaking): for each es array.push

---
+++ week-02/day-2/array02-matchmaking.ts
@@ -0,0 +1,19 @@
+'use strict';
+// Write a function that joins two array by matching one girl with one boy in a new array
+// Exepected output: ["Eve", "Joe", "Ashley", "Fred"...]
+
+let girls: string[] = ['Eve', 'Ashley', 'Claire', 'Kat', 'Jane', 'Mari', 'Bori'];
+let boys: string[] = ['Joe', 'Fred', 'Tom', 'Todd', 'Neef', 'Jeff', 'Jani'];
+
+function beTogether(array, array2){
+    let newarray: string[] = []
+    array.forEach((elem, index) => {
+        newarray.push(array[index]);
+        if(array2[index] != undefined){
+            newarray.push(array2[index])    
+    }});
+    return newarray;
+}
+
+console.log(beTogether(girls, boys));
+export = beTogether;
