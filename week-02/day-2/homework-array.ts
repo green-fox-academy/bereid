b932fa5a78e52cac440becb0ec2f1f2c8bb37563
bereid <berei.daniel@gmail.com>
2018-08-07 09:46:24 +0200

    modified(homework-array): added some array methods with examples

--- week-02/day-2/homework-array.ts
+++ week-02/day-2/homework-array.ts
@@ -22,5 +22,56 @@ array2.push([25,26,27]);
 
 console.log(array2[5][2]);
 
-// ide jojjenek az array methodok
+// array.concat
+
+let concatstring = "Hello, my friend, "
+let concatstring2 = "we support Liverpool!"
+console.log(concatstring.concat(concatstring2));
+
+// array.every
+
+let myFolder = [9, 10, 11, 21, 8, 6, 34]
+
+function isBigEnough(element, index, array){
+    return (element >= 10);
+}
+
+let myFolder2 = myFolder.every(isBigEnough);
+console.log(myFolder2);
+
+// array.filter
+
+let myFolder3 = [2, 3, 4, 5, 6, 7, 8, 1123];
+
+function isLittleEnough(element, index, array){
+    return (element <= 5);
+}
+
+let myFolder4 = myFolder3.filter(isLittleEnough);
+console.log(myFolder4);
+
+// array.map
+
+let numFolder = [3, 4, 5];
+
+function double(element){
+    return element * 2;
+}
+
+let numFolder2 = numFolder.map(double);
+console.log(numFolder2);
+
+// array.forEach
+
+let numberFolder = [5, 6, 7];
+
+numberFolder.forEach(function (value){
+    console.log(value);
+});
+
+// array.indexOf
+
+let numberFolder2 = [12, 5, 8, 130, 44];
+let indexOfNumberFolder = numberFolder2.indexOf(130);
+console.log(indexOfNumberFolder);
 
