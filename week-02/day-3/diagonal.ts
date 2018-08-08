1e9aa291136a8baeb1a070e64cd6dad43aed2825
Fanni <fanni@Bereis-MacBook-Air.local>
2018-08-08 14:18:21 +0200

    added(colored box, diagonal, line in the middle, to the center): first four task is ready, lets rock :squirrel:

---
+++ week-02/day-3/diagonal.ts
@@ -0,0 +1,18 @@
+// DO NOT TOUCH THE CODE ABOVE THIS LINE
+// Draw the canvas' diagonals.
+// If it starts from the upper-left corner it should be green, otherwise it should be red.
+
+const canvas = document.querySelector('.main-canvas') as HTMLCanvasElement;
+const ctx = canvas.getContext('2d');
+
+ctx.beginPath();
+ctx.strokeStyle = "green";
+ctx.moveTo(0, 0);
+ctx.lineTo(600, 400);
+ctx.stroke();
+
+ctx.beginPath();
+ctx.strokeStyle = "red";
+ctx.moveTo(0, 400);
+ctx.lineTo(600, 0);
+ctx.stroke();
---
+++ week-02/day-3/to-the-center.ts
@@ -0,0 +1,22 @@
+// DO NOT TOUCH THE CODE ABOVE THIS LINE
+// Create a line drawing function that takes 2 parameters:
+// The x and y coordinates of the line's starting point
+// and draws a line from that point to the center of the canvas
+// Draw 3 lines with that function. Use loop for that.
+
+const canvas = document.querySelector('.main-canvas') as HTMLCanvasElement;
+const ctx = canvas.getContext('2d');
+
+function lineDraw(x,y){
+    ctx.beginPath();
+    ctx.strokeStyle = "blue";
+    ctx.moveTo(x, y);
+    ctx.lineTo(canvas.width / 2, canvas.height / 2);
+    ctx.stroke();
+    }
+
+function threeLineDraw (step){
+    for(let i = 0; i <= 100; i++){
+        lineDraw(step * i, 0);
+    }
+}
