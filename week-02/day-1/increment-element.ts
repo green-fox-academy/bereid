8a69a16629fc678feddd4787ce2ee779e5525f5f
Fanni <fanni@192.168.1.11>
2018-08-12 18:14:03 +0200

    modified (increment elements): changed the code after understand .map method

--- week-02/day-1/increment-element.ts
+++ week-02/day-1/increment-element.ts
@@ -4,26 +4,6 @@ export {}
 // -  Increment the third element simply by accessing it
 // -  Log the third element to the console
 
-/*let numList = [1,2,3,4,5]
-let answer = numList.map(function (num){
-    if(num % 3 == 0){
-        return num + 1;
-    }
-    else {
-        return num;
-    }
-})
-console.log(answer);
-*/
-
-/* masik megoldas
-
-let numList = [1,2,3,4,5]
-numList[2]++;
-console.log(numList);
-
-*/
-
 let numList = [1,2,3,4,5]
 
 numList.map(function (num) {
