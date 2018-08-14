3b2140ddc43e063f64f8ffda82b1029968006173
Fanni <fanni@Bereis-MacBook-Air.local>
2018-08-14 14:03:47 +0200

    added(count-lines): still very easy, syllabus pleeeaase

---
+++ week-03/day-2/count-lines.ts
@@ -0,0 +1,21 @@
+// Write a function that takes a filename as string,
+// then returns the number of lines the file contains.
+// It should return zero if it can't open the file, and
+// should not raise any error.
+'use strict'
+export { };
+declare function require(path: string): any;
+const fs = require("fs");
+const charBase: string = "utf-8";
+
+function countLines(fileName: string): number {
+  try {
+    let lengthOfFile: number = fs.readFileSync(fileName, charBase).split("\n").length
+    return lengthOfFile;
+  }
+  catch (e) {
+    return 0;
+  }
+}
+
+console.log(countLines("he.txt"));
