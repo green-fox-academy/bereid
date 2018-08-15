9e7082204e585f30c4366a84ed54164a299a6db8
Daniel Berei <berei.daniel@gmail.com>
2018-08-15 21:17:16 +0200

    added(counter) exciting excercise, created a class with three methods, one of them with if statement

---
+++ week-03/day-3/counter.ts
@@ -0,0 +1,38 @@
+// Create a Counter class
+// which has an integer property
+// when creating it should have a default value 0 or we can specify it when creating
+// we can add(number) to increase the counter's value by a whole number
+// or we can add() without parameters just increasing the counter's value by one
+// and we can get() the current value as string
+// also we can reset() the value to the initial value
+
+class Counter {
+  private counterNum: number = 0;
+  constructor(counterNum: number){
+    this.counterNum = counterNum;
+  }
+  add(addNumber?: any){
+    if(addNumber > 0){
+      this.counterNum += addNumber;
+    }else{
+      this.counterNum ++;
+    }
+  }
+  get(){
+    let currentCounterNum = this.counterNum.toString();
+    return currentCounterNum;
+    console.log(currentCounterNum);
+  }
+  reset(){
+    this.counterNum -= this.counterNum;
+    return this.counterNum;
+    console.log(this.counterNum);
+  }
+}
+
+let newNumber: Counter = new Counter(100);
+newNumber.add();
+newNumber.add(132);
+newNumber.get();
+newNumber.reset();
+console.log(newNumber);
