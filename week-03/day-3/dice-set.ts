929fdd4cfbad5adac607ac4aa5e5de2c5f3ac3f0
Daniel Berei <berei.daniel@gmail.com>
2018-08-16 20:04:00 +0200

    add(dice-set): woooooow

--- week-03/day-3/dice-set.ts
+++ week-03/day-3/dice-set.ts
@@ -5,48 +5,57 @@
 // Your task is to roll the dices until all of the dices are 6
 
 class DiceSet {
-      dices: number[];
-      readonly numOfDices: number = 6;
-  
-      roll(): number[] {
-          this.dices = [];
-          for(var i = 0; i < this.numOfDices; i++) { 
-              this.dices.push(Math.floor(Math.random() * 6 + 1));
-          }
-          return this.dices;
-      }
-  
-      reroll(index?: number) {
-          if(index == undefined) {
-              for(var i = 0; i < this.numOfDices; i++) { 
-                  this.dices[i] = Math.floor(Math.random() * 6 + 1);
-              }
-          } else {
-              this.dices[index] = Math.floor(Math.random() * 6 + 1);
-          }
-      }
-      
-      getCurrent(index?: number) {
-          if(index == undefined) {
-              for(var i = 0; i < this.numOfDices; i++) { 
-                  console.log(this.dices[i]);
-              }
-          } else {
-              console.log(this.dices[index]);
-          }        
-      }
-  }
-  
-  let diceSet = new DiceSet();
-  diceSet.roll();
-  diceSet.getCurrent();
-  console.log("------------------");
-  diceSet.reroll();
-  diceSet.getCurrent();
-  console.log("------------------");
-  diceSet.getCurrent(5);
-  diceSet.reroll();
-  diceSet.getCurrent();
-  console.log("------------------");
-  diceSet.reroll(4);
-  diceSet.getCurrent();
+    dices: number[];
+    readonly numOfDices: number = 6;
+
+    roll(): number[] {
+        this.dices = [];
+        for (var i = 0; i < this.numOfDices; i++) {
+            this.dices.push(Math.floor(Math.random() * 6 + 1));
+        }
+        return this.dices;
+    }
+
+    reroll(index?: number) {
+        if (index == undefined) {
+            for (var i = 0; i < this.numOfDices; i++) {
+                this.dices[i] = Math.floor(Math.random() * 6 + 1);
+            }
+        } else {
+            this.dices[index] = Math.floor(Math.random() * 6 + 1);
+        }
+    }
+
+    getCurrent(index?: number) {
+        if (index == undefined) {
+            for (var i = 0; i < this.numOfDices; i++) {
+                console.log(this.dices[i]);
+            }
+        } else {
+            console.log(this.dices[index]);
+        }
+    }
+}
+
+let diceSet = new DiceSet();
+diceSet.roll();
+diceSet.getCurrent();
+
+function is6(value: number) {
+    return value === 6
+}
+
+function findFirstIndex(array: number[]): number {
+    for (let i = 0; i < array.length; i++) {
+        if (!is6(array[i])) {
+            return i;
+        }
+    }
+    return -1;
+}
+
+while (!diceSet.dices.every(is6)) {
+    console.log("Before reroll: " + diceSet.getCurrent());
+    diceSet.reroll(findFirstIndex(diceSet.dices));
+    console.log("After reroll: " + diceSet.getCurrent());
+}
