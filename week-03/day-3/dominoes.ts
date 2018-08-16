084dc3ace9a0ef406352369fb9ec806561fde228
Daniel Berei <berei.daniel@gmail.com>
2018-08-16 18:50:04 +0200

    mod(dominoes): new function with while loop works well

--- week-03/day-3/dominoes.ts
+++ week-03/day-3/dominoes.ts
@@ -7,11 +7,11 @@ import { Domino } from "./domino";
 function initializeDominoes(): Domino[] {
     let dominoes = [];
     dominoes.push(new Domino(5, 2));
-    dominoes.push(new Domino(1, 5));
     dominoes.push(new Domino(2, 4));
-    dominoes.push(new Domino(7, 1));
     dominoes.push(new Domino(6, 7));
     dominoes.push(new Domino(4, 6));
+    dominoes.push(new Domino(1, 5));
+    dominoes.push(new Domino(7, 1));
     return dominoes;
 }
 
@@ -19,18 +19,20 @@ function sortDominoes(dominoes: Domino[]): Domino[] {
     let sortedDominoes: Domino[] = [];
     sortedDominoes.push(dominoes[0]);
     dominoes.splice(0, 1);
-    for (let i = 0; i < dominoes.length; i++) {
-        if (dominoes[i].values[0] === sortedDominoes[sortedDominoes.length - 1].values[1]) {
-            sortedDominoes.push(dominoes[i]);
-        }
-        else if (dominoes[i].values[1] === sortedDominoes[0].values[0]) {
-            sortedDominoes.unshift(dominoes[i]);
+    while (dominoes.length > 0) {
+        for (let i = 0; i < dominoes.length; i++) {
+            if (dominoes[i].values[0] === sortedDominoes[sortedDominoes.length - 1].values[1]) {
+                sortedDominoes.push(dominoes[i]);
+                dominoes.splice(i, 1);
+            }
+            else if (dominoes[i].values[1] === sortedDominoes[0].values[0]) {
+                sortedDominoes.unshift(dominoes[i]);
+                dominoes.splice(i, 1);
+            }
         }
     }
     return sortedDominoes;
 }
-
-
 
 function print(dominoes: Domino[]) {
     dominoes.forEach(function (value) {
@@ -40,3 +42,29 @@ function print(dominoes: Domino[]) {
 
 let dominoes = initializeDominoes();
 print(sortDominoes(dominoes));
+
+function sortDominoes2(dominoes: Domino[]): Domino[] {
+    let sortedDominoes: Domino[] = [];
+    sortedDominoes.push(dominoes[0]);
+    dominoes.splice(0, 1);
+    while (dominoes.length > 0) {
+        let dominoOutIndex: number;
+        for (let i = 0; i < dominoes.length; i++) {
+            if (dominoes[i].values[0] === sortedDominoes[sortedDominoes.length - 1].values[1]) {
+                sortedDominoes.push(dominoes[i]);
+                dominoOutIndex = i;
+                break;
+            }
+            else if (dominoes[i].values[1] === sortedDominoes[0].values[0]) {
+                sortedDominoes.unshift(dominoes[i]);
+                dominoOutIndex = i;
+                break;
+            }
+        }
+        dominoes.splice(dominoOutIndex, 1);
+    }
+    return sortedDominoes;
+}
+
+let dominoes2 = initializeDominoes();
+print(sortDominoes2(dominoes2));
