fd92af7a9bfa288e5e580c240a058c82e6d3e356
Daniel Berei <berei.daniel@gmail.com>
2018-08-22 16:24:17 +0200

    added(flyable): added flyable interface to bird inherited class

--- week-04/day-3/zoo/bird.ts
+++ week-04/day-3/zoo/bird.ts
@@ -1,6 +1,7 @@
 import { Animal } from "./zoo";
+import { Flyable } from "../flyable/flyable";
 
-export class Bird extends Animal {
+export class Bird extends Animal implements Flyable {
   canFly: boolean;
   isProtectedSpecies: boolean;
   wingSize: number;
@@ -16,5 +17,17 @@ export class Bird extends Animal {
 
   breed() {
     return "by laying eggs."
+  }
+
+  land() {
+    return "I landed";
+  }
+
+  fly() {
+    return "Wow, I'm flying!";
+  }
+
+  takeOff() {
+    return "Now I will fly!";
   }
 }
