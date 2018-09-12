ddc1e3d256d68974f44b352a79395b2fd49510ca
Daniel Berei <berei.daniel@gmail.com>
2018-09-12 12:13:34 +0200

    add(queryselector-1 html and js)

---
+++ week-06/day-3/queryselector-1.js
@@ -0,0 +1,23 @@
+// You can work in the html or in a separate js file. Like:
+
+// 1. store the element that says 'The King' in the 'king' variable.
+// console.log it.
+// 2. store 'The Businessman' and 'The Lamplighter'
+// in the 'businessLamp' variable.
+// console.log each of them.
+// 3. store 'The King' and 'The Conceited Man'
+// in the 'conceitedKing' variable.
+// alert them one by one.
+// 4. store 'The King', 'The Conceited Man' and 'The Lamplighter'
+// in the 'noBusiness' variable.
+// console.log each of them.
+
+let king = document.getElementById('b325');
+console.log(king);
+
+let businessLamp = document.getElementsByClassName('big');
+console.log(businessLamp[0], businessLamp[1]);
+
+let conceitedKing = document.querySelectorAll('.container .asteroid')
+console.log(conceitedKing[0], conceitedKing[1]);
+
