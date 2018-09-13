f95f5c944f0ee4086ae666fb1f00bfaaf3b72b6c
Daniel Berei <berei.daniel@gmail.com>
2018-09-13 14:32:59 +0200

    added(nodejs project, ejs template, jsons, express, ejs)

---
+++ week-06/day-4/greetings/index.js
@@ -0,0 +1,17 @@
+'use strict';
+const express = require('express');
+const path = require('path');
+const app = express();
+const PORT = 3000;
+
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+    res.render('home', {
+        title: 'home',
+    });
+})
+
+app.listen(PORT, () => {
+    console.log(`Server is listening to PORT: ${PORT}`);
+})
