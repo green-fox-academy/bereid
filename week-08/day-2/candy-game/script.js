a0ede804fd14ad8132b014c97016887c83f91ae8
Daniel Berei <berei.daniel@gmail.com>
2018-09-25 16:09:08 +0200

    feat(creates 1 candy if you have more than 9 lollypops)

--- week-08/day-2/candy-game/script.js
+++ week-08/day-2/candy-game/script.js
@@ -1,19 +1,32 @@
 const create = document.querySelector('.create-candies');
 const buy = document.querySelector('.buy-lollypops');
 let candies = document.querySelector('.candies');
-let numberOfCandies = 0;
+let numberOfCandies = 10000;
+candies.innerHTML = numberOfCandies;
+let lollypops = 3;
 
 const giveACandy = () => {
-  numberOfCandies ++;
+  numberOfCandies++;
   candies.innerHTML = numberOfCandies;
 }
 
 const buyALollypop = () => {
-  if (numberOfCandies >= 100){
+  if (numberOfCandies >= 100) {
     numberOfCandies = numberOfCandies - 100;
     candies.innerHTML = numberOfCandies;
+    lollypops++;
   }
-  
+}
+
+const candyGenerator = () => {
+  setInterval(candyForLollypop, 1000)
+}
+
+const candyForLollypop = () => {
+  if (lollypops % 10 === 0) {
+    numberOfCandies += lollypops / 10;
+    candies.innerHTML = numberOfCandies;
+  }
 }
 
 create.addEventListener('click', () => {
@@ -23,3 +36,5 @@ create.addEventListener('click', () => {
 buy.addEventListener('click', () => {
   buyALollypop();
 });
+
+candyGenerator();
