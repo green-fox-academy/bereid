383f1a5a6ddf94d958ef84ab3904d27e2058b2db
Daniel Berei <berei.daniel@gmail.com>
2018-10-04 12:11:31 +0200

    add(basic server): invite msql and renders index.html for / endpoint

---
+++ week-09/day-4/quiz/server.js
@@ -0,0 +1,34 @@
+'use strict';
+
+const express = require('express');
+const app = express();
+const PORT = 8080;
+const mysql = require('mysql');
+const bodyParser = require('body-parser');
+const jsonParser = bodyParser.json();
+const path = require('path');
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'Bereid+123',
+  database: 'quiz',
+});
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use('/assets', express.static('assets'));
+
+connection.connect((err) => {
+  if (err) {
+    console.log('Connection failed to database: ' + err);
+    return;
+  }
+  console.log('Connection established');
+});
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '/assets/index.html'))
+});
+
+app.listen(PORT, () => {
+  console.log(`App is running on port ${PORT}`);
+});
