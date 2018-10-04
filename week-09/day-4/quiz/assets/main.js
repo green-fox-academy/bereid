0466e1a9b76a0ec627c6589f04eb4f43f3baab1e
Daniel Berei <berei.daniel@gmail.com>
2018-10-04 14:58:34 +0200

    feat(form renders with question and answers

--- week-09/day-4/quiz/assets/main.js
+++ week-09/day-4/quiz/assets/main.js
@@ -1,7 +1,10 @@
 window.onload = () => {
   let wrapper = document.querySelector('.wrapper');
-  let newP = document.createElement('p');
   let URL = 'http://localhost:8080'
+  let form = document.createElement('form');
+  wrapper.appendChild(form);
+  let questionField = document.createElement('legend');
+  form.appendChild(questionField).innerHTML = 
   fetch(`${URL}/game`, {
     method: 'get',
     headers: {
@@ -10,8 +13,21 @@ window.onload = () => {
   })
     .then(resp => resp.json())
     .then(response => {
-      newP.innerText = response[0].question;
-      wrapper.appendChild(newP)
+      console.log(response);
+      form.appendChild(questionField).innerHTML = response.question;
+      response.answers.forEach(answer => {
+        let newAnswer = document.createElement('input');
+        newAnswer.type = 'radio';
+        newAnswer.value = answer.id;
+        newAnswer.id = answer.id;
+        form.appendChild(newAnswer);
+        let answerLabel = document.createElement('label');
+        answerLabel.htmlFor = answer.id;
+        answerLabel.innerText = answer.answer;
+        form.appendChild(answerLabel);
+        
+        
+      });
     });
   ;
 }
